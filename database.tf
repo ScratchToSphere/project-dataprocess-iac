3e675a81ad7e6831672ee4dd6e2ad609e09a7170
Thomas Ovigny <thomasovigny.pro@gmail.com>
2025-11-24 23:26:54 +0100

    Switching from AuroraDB to RDS PostgreSQL due to free tier limitations.

--- database.tf
+++ database.tf
@@ -14,32 +14,34 @@ resource "aws_db_subnet_group" "aurora_subnet_group" {
   }
 }
 
-# Create the Aurora Serverless Cluster
-resource "aws_rds_cluster" "aurora_cluster" {
-  cluster_identifier     = "dataprocess-cluster"
-  engine                 = "aurora-postgresql"
-  engine_mode            = "provisioned" # Prerequisite for Serverless v2
-  engine_version         = "14.6"        # Compatible versiob Serverless v2
-  database_name          = "dataprocessdb"
-  master_username        = "adminuser"
-  master_password        = random_password.db_password.result
+# Create the PostgreSQL RDS instance
+resource "aws_db_instance" "postgres_instance" {
+  identifier             = "dataprocess-db"
   
-  # Network & Security
+  # Engine & Version
+  engine                 = "postgres"
+  engine_version         = "16.10" 
+  instance_class         = "db.t3.micro" # Free Tier Eligible
+  
+  # Storage (required)
+  allocated_storage      = 20    # 20 GB (free tier eligible)
+  storage_type           = "gp2"
+  
+  # Login & DB Name
+  db_name                = "dataprocessdb"
+  username               = "adminuser"
+  password               = random_password.db_password.result
+  
+  # Réseau & Sécurité
   db_subnet_group_name   = aws_db_subnet_group.aurora_subnet_group.name
   vpc_security_group_ids = [aws_security_group.aurora_sg.id]
-  skip_final_snapshot    = true # Easily destroyable for the lab (PUT FALSE in prod!)
+  
+  # Lab specific settings
+  publicly_accessible    = false # Security: not publicly accessible
+  skip_final_snapshot    = true  # Destroy without snapshot for lab purposes
+  multi_az               = false # False due to Free Tier limitations (Multi-AZ in prod !)
 
-  # Serverless v2 configuration (cost optimization for the lab)
-  serverlessv2_scaling_configuration {
-    max_capacity = 1.0 # Max 1 ACU (Enough for the labo)
-    min_capacity = 0.5 # Min 0.5 ACU (Most affordable)
+  tags = {
+    Name = "DataProcess-PostgreSQL"
   }
-}
-
-
-resource "aws_rds_cluster_instance" "aurora_instance" {
-  cluster_identifier = aws_rds_cluster.aurora_cluster.id
-  instance_class     = "db.serverless" # Magic !
-  engine             = aws_rds_cluster.aurora_cluster.engine
-  engine_version     = aws_rds_cluster.aurora_cluster.engine_version
 }
